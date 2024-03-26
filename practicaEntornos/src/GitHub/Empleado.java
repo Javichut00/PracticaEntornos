f3986062315d824e1127c30e22e34676b8c9069b
javin <javin@192.168.56.1>
2024-03-26 22:42:12 +0100

    Clases creadas

---
+++ practicaEntornos/src/GitHub/Empleado.java
@@ -0,0 +1,21 @@
+package GitHub;
+
+public class Empleado extends Persona {
+	
+	private String cargo;
+    private double salario;
+
+    public Empleado(String nombre, int edad, String cargo, double salario) {
+        super(nombre, edad);
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
