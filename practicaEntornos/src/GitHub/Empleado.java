cf073b1df159e2d670c563d7c0b51f5eb6cdd184
javin <javin@192.168.56.1>
2024-03-27 20:02:22 +0100

    Añadimos los atributos

--- practicaEntornos/src/GitHub/Empleado.java
+++ practicaEntornos/src/GitHub/Empleado.java
@@ -2,20 +2,5 @@ package GitHub;
 
 public class Empleado extends Persona {
 	
-	private String cargo;
-    private double salario;
-
-    public Empleado(String nombre, int edad, String cargo, double salario) {
-        super(nombre, edad);
-        this.cargo = cargo;
-        this.salario = salario;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
+	
 }
--- practicaEntornos/src/GitHub/Persona.java
+++ practicaEntornos/src/GitHub/Persona.java
@@ -2,19 +2,5 @@ package GitHub;
 
 public class Persona {
 	
-	private String nombre;
-    private int edad;
 
-    public Persona(String nombre, int edad) {
-        this.nombre = nombre;
-        this.edad = edad;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
 }
