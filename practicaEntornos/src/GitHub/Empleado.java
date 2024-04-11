269cda2a1244124687a446d64435f21b22ec9601
javin <javin@192.168.56.1>
2024-04-11 19:41:08 +0200

    Comentarios JavaDoc

--- practicaEntornos/src/GitHub/Empleado.java
+++ practicaEntornos/src/GitHub/Empleado.java
@@ -1,9 +1,24 @@
 package GitHub;
 
+	/**
+	 * Esta clase otorga una persona pero de tipo Empleado con sus propios atributos
+	 * @author javin
+	 * @version 1.0
+	 * @since 31/03/2024
+	 */
+
 public class Empleado extends Persona {
 	
 	private String cargo;
     private double salario;
+    
+    /**
+     * Constructor para crear un objeto Empleado con un nombre, una edad, un cargo y un salario.
+     * @param nombre El nombre del empleado.
+     * @param edad La edad del empleado.
+     * @param cargo El cargo del empleado.
+     * @param salario El salario del empleado.
+     */
     
     public Empleado(String nombre, int edad, String cargo, double salario) {
         super(nombre, edad);
@@ -11,17 +26,36 @@ public class Empleado extends Persona {
         this.salario = salario;
     }
     
+    /**
+     * Obtiene el cargo del empleado.
+     * @return El cargo del empleado.
+     */
+    
     public String getCargo() {
         return cargo;
     }
-
+    
+    /**
+     * Obtiene el salario del empleado.
+     * @return El salario del empleado.
+     */
+    
     public double getSalario() {
         return salario;
     }
     
+    /**
+     * Simula que el empleado está trabajando imprimiendo un mensaje en la consola.
+     */
+    
     public void trabajar() {
         System.out.println("Soy " + getCargo() + " y estoy trabajando.");
     }
+    
+    /**
+     * Recibe un aumento en el salario del empleado.
+     * @param aumento El aumento en el salario.
+     */
     
     public void recibirAumento(double aumento) {
         salario += aumento;
--- practicaEntornos/src/GitHub/Persona.java
+++ practicaEntornos/src/GitHub/Persona.java
@@ -1,26 +1,57 @@
 package GitHub;
+	
+	/**
+	 * Esta clase otorga una persona con un nombre y una edad que realiza algunas acciones de persona
+	 * @author javin
+	 * @version 1.0
+	 * @since 31/03/2024
+	 */
 
 public class Persona {
 	
 	private String nombre;
     private int edad;
     
+    /**
+     * Constructor para crear un objeto Persona con un nombre y una edad.
+     * @param nombre El nombre de la persona.
+     * @param edad La edad de la persona.
+     */
+    
     public Persona(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
     }
     
+    /**
+     * 
+     * @return Devuelve el nombre de persona
+     */
+    
     public String getNombre() {
         return nombre;
     }
-
+    
+    /**
+     * 
+     * @return Devuelve la edad de persona
+     */
+    
     public int getEdad() {
         return edad;
     }
     
+    /**
+     * Este metodo muestra un mensaje en el que la persona saluda diciendo su nombre
+     */
+    
     public void saludar() {
         System.out.println("Hola, soy " + nombre);
     }
+    
+    /**
+     * Este metodo incrementa la edad cada vez que cumple años y devuelve un mensaje sobre su nueva edad
+     */
     
     public void cumplirAnios() {
         edad++;
